608d42bcd25bbc1cceb8ab24c09dc19c8c63b301
SarikViper <dzamolovsardor5@gmail.com>
2025-03-08 23:10:14 +0500

    added search func

--- src/App.tsx
+++ src/App.tsx
@@ -13,15 +13,12 @@ import Tasks from "./components/custom/Tasks.tsx";
 function App() {
 
   const [todos, setTodos] = useState<Array<Todo>>([]);
-
-
-
   return (
     <>
     <div className="w-full h-screen bg-black text-white">
     <ResizablePanelGroup className="w-full h-screen" direction="horizontal">
       <ResizablePanel>
-        <Search/>
+        <Search todos = {todos} setTodos = {setTodos}/>
         <Tasks todos={todos}/>
       </ResizablePanel>
       <ResizableHandle withHandle />
--- src/components/custom/Search.tsx
+++ src/components/custom/Search.tsx
@@ -1,12 +1,38 @@
+import React, { useState } from "react";
 import { Button } from "..//ui/button";
 import { Input } from "../ui/input";
+import { Todo } from "@/types";
 
-const Search = () => {
+interface SearchFormProps {
+  todos: Array<Todo>;
+  setTodos: (array: any) => void;
+}
+
+const Search: React.FC<SearchFormProps> = ({ todos, setTodos }) => {
+  const [search, setSearch] = useState<string>("");
+  const searchTask = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setSearch(value);
+
+    if (value.trim() === "") {
+      setTodos(todos);
+    } else {
+      const filtered = todos.filter((todo) =>
+        todo.title.toLowerCase().includes(value.toLowerCase().trim())
+      );
+      setTodos(filtered);
+    }
+  };
   return (
     <>
       <div className="flex w-full max-w-sm items-center space-x-2">
-        <Input type="email" placeholder="Search Task" />
-        <Button variant="outline" className="hover:text-white hover:bg-gray-500 bg-black cursor-pointer">Search</Button>
+        <Input type="text" placeholder="Search Task" onChange={searchTask} />
+        <Button
+          variant="outline"
+          className="hover:text-white hover:bg-gray-500 bg-black cursor-pointer"
+        >
+          Search
+        </Button>
       </div>
     </>
   );
