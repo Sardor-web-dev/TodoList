f0d08cbb947174f7fa8f2cca885489076f4a3d7a
SarikViper <dzamolovsardor5@gmail.com>
2025-03-08 22:00:31 +0500

    added form

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,55 @@
-import { Button } from "@/components/ui/button"
+import { Todo } from "./types.tsx"
+import { useState } from "react"
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable"
+  
 
 function App() {
+
+  const [todos, setTodos] = useState<Array<Todo>>([]);
+
+  function handleSubmit (e: any) {
+    
+    e.preventDefault();
+
+    const fm = new FormData(e.currentTarget)
+
+    const title = fm.get("title") as string
+
+    const newTodo: Todo = {
+      id:Math.random(),
+      title,
+      completed: false,
+      added_at: new Date().toISOString(),
+    }
+
+    setTodos([...todos, newTodo]);
+
+
+    console.log(todos);
+    
+  }
+
+
   return (
-    <div className="flex flex-col items-center justify-center min-h-svh">
-      <Button>Click me</Button>
+    <>
+    <div className="w-full h-screen bg-black text-white">
+    <ResizablePanelGroup className="w-full h-screen" direction="horizontal">
+      <ResizablePanel>One</ResizablePanel>
+      <ResizableHandle withHandle />
+      <ResizablePanel>
+        <form onSubmit={handleSubmit}>
+          <input type="text"  name="title" />
+          <button>add</button>
+        </form>
+      </ResizablePanel>
+    </ResizablePanelGroup>
     </div>
-  )
+    </>
+)
 }
 
 export default App
---
+++ src/types.ts
@@ -0,0 +1,6 @@
+export type Todo = {
+    id: string;
+    title: string;
+    completed: boolean;
+    added_at:string;
+}
