2f219d68b8b0683c73d3e893ee53cf8158e9f023
SarikViper <dzamolovsardor5@gmail.com>
2025-03-09 13:58:42 +0500

    some changes

--- src/components/custom/FormAdd.tsx
+++ src/components/custom/FormAdd.tsx
@@ -1,49 +1,54 @@
-import { Todo } from "../../types.ts"
+import { Todo } from "../../types.ts";
 import { Input } from "../ui/input";
 import { Button } from "..//ui/button";
 
-
 interface SearchFormProps {
-    todos: Array<Todo>;
-    setTodos: (array: any) => void;
-  }
+  todos: Array<Todo>;
+  setTodos: (array: any) => void;
+}
 
-const FormForAdd:React.FC<SearchFormProps> = ({todos, setTodos}) => {
-    
-  function handleSubmit (e: any) {
-    
+const FormForAdd: React.FC<SearchFormProps> = ({ todos, setTodos }) => {
+  function handleSubmit(e: any) {
     e.preventDefault();
 
-    const fm = new FormData(e.currentTarget)
+    const fm = new FormData(e.currentTarget);
 
-    const title = fm.get("title") as string
-    const completed = fm.get("isCompleted") as string
+    const title = fm.get("title") as string;
+    const completed = fm.get("isCompleted") as string;
 
     const newTodo: Todo = {
-      id:Math.random(),
+      id: Math.random(),
       title,
       completed,
-      added_at: new Date().toDateString()
-    }
+      added_at: new Date().toDateString(),
+    };
 
     setTodos([...todos, newTodo]);
 
-
     console.log(todos);
-    
   }
   return (
     <>
       <form onSubmit={handleSubmit}>
         <Input placeholder="Task title" name="title" />
         <select name="isCompleted">
-          <option className="text-white bg-black rounded-2xl gap-2 " value="false">Not Completed</option>
-          <option className="text-white bg-black rounded-2xl gap-2 " value="true">Completed</option>
+          <option
+            className="text-white bg-black rounded-2xl gap-2 "
+            value="false"
+          >
+            Not Completed
+          </option>
+          <option
+            className="text-white bg-black rounded-2xl gap-2 "
+            value="true"
+          >
+            Completed
+          </option>
         </select>
-        <Button>Add New Task</Button>  
+        <Button>Add New Task</Button>
       </form>
     </>
   );
-}
+};
 
 export default FormForAdd;
